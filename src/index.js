b45b4a6b3415bd0620c9eeb8c60f945d1ec670c7
Your Name <you@example.com>
2023-09-14 20:28:50 +0700

    1 - Thiết kế web database và connect tới mongodb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,17 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+mongoose
+  .connect(
+    `mongodb+srv://trantung310502:${process.env.MONGO_DB}@cluster0.hafunfl.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connect Db success!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log("Server is running in port: " + port);
