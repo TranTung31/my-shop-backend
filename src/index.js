0048437485e0615631c2c2d8ab46d98b3669fd62
Your Name <you@example.com>
2023-09-18 13:16:09 +0700

    2 - Phân chia theo MVC ở backend

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { default: mongoose } = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(bodyParser.json());
+
+routes(app);
 
 mongoose
-  .connect(
-    `mongodb+srv://trantung310502:${process.env.MONGO_DB}@cluster0.hafunfl.mongodb.net/?retryWrites=true&w=majority`
-  )
+  .connect(`${process.env.MONGO_DB}`)
   .then(() => {
     console.log("Connect Db success!");
   })
