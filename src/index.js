dd75d552ecb7ea690d86f7c81ec8276875c65dca
Your Name <you@example.com>
2024-03-14 23:07:38 +0700

    BE - Thay đổi file index.js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { default: mongoose } = require("mongoose");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const ConfigMongoDB = require("./config/mongodb");
+
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3001;
+const START_SERVER = () => {
+  const app = express();
+  const port = process.env.PORT || 3001;
 
-// Sử dụng middleware cors
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-// app.use(
-//   express.static("public", {
-//     maxAge: 3600000, // 1 giờ
-//     setHeaders: function (res, path, stat) {
-//       res.set("Cache-Control", "public");
-//     },
-//   })
-// );
+  // Sử dụng middleware cors
+  app.use(cors());
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ limit: "50mb" }));
+  app.use(bodyParser.json());
+  app.use(cookieParser());
 
-routes(app);
+  routes(app);
 
-mongoose
-  .connect(`${process.env.MONGO_DB}`)
-  .then(() => {
-    console.log("Connect Db success!");
-  })
-  .catch((err) => {
-    console.log(err);
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
   });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+  app.listen(port, () => {
+    console.log(`3. Back-end Server is running at http://localhost:${port}`);
+  });
+};
 
-app.listen(port, () => {
-  console.log("Server is running in port: " + port);
-});
+(async () => {
+  try {
+    console.log("1. Connect to Database");
+    await ConfigMongoDB.CONNECT_DB();
+    START_SERVER();
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+})();
